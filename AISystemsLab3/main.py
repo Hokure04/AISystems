a3daa7677b22a4d8367aad18673277df21f76c71
ASUS <hokure.04@gmail.com>
2024-10-23 20:29:37 +0300

    normalization and coding of categorical features was added

--- AISystemsLab3/main.py
+++ AISystemsLab3/main.py
@@ -1,15 +1,7 @@
 import pandas as pd
+import numpy as np
 from math import sqrt
 from prettytable import PrettyTable
-
-df = pd.read_csv('Student_Performance.csv')
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-print(df)
-df = df.drop(columns=['Extracurricular Activities'])
-df = df.apply(pd.to_numeric, errors='coerce')
-df = df.dropna()
-performance_data = df['Performance Index']
 
 def calc_mean(data):
     return sum(data) / len(data)
@@ -20,19 +12,68 @@ def calc_variance(data, mean):
 def calc_standard_deviation(variance):
     return sqrt(variance)
 
-table = PrettyTable()
-table.field_names = ["column","mean","variance","standard deviation", "min value", "max value", "0.25 Quantile","0.5 Quantile","0.75 Quantile"]
+def min_max_normalize(column):
+    return (column - column.min()) / (column.max() - column.min())
 
-for column in df.columns:
-    data = df[column]
-    mean_value = calc_mean(data)
-    variance_value = calc_variance(performance_data, mean_value)
-    std_value = calc_standard_deviation(variance_value)
-    min_value = min(data)
-    max_value = max(data)
-    q25 = data.quantile(0.25)
-    q50 = data.quantile(0.5)
-    q75 = data.quantile(0.75)
-    table.add_row([column, mean_value, variance_value, std_value, min_value, max_value, q25, q50, q75])
+def z_normalize(column):
+    mean = calc_mean(column)
+    std_dev = calc_standard_deviation(calc_variance(column, mean))
+    return (column - mean) / std_dev
 
-print(table)
+def calculating_dataset_characteristics(df, table):
+    for column in df.columns:
+        data = df[column]
+        mean_value = calc_mean(data)
+        variance_value = calc_variance(data, mean_value)
+        std_value = calc_standard_deviation(variance_value)
+        min_value = min(data)
+        max_value = max(data)
+        quantile_25 = np.percentile(data, 25)
+        quantile_50 = np.percentile(data, 50)
+        quantile_75 = np.percentile(data, 75)
+        table.add_row([column, mean_value, variance_value, std_value, min_value, max_value, quantile_25, quantile_50,
+                       quantile_75])
+    print(table)
+
+def main():
+    df = pd.read_csv('Student_Performance.csv')
+    # print(df)
+    df['Extracurricular Activities'] = df['Extracurricular Activities'].map({'Yes':1, 'No':0}) # кодирование категориальных признаков
+
+    df = df.apply(pd.to_numeric, errors='coerce')
+    df.fillna(0, inplace=True) # на место пропуска вставляем 0
+
+
+    table = PrettyTable()
+    table.field_names = ["column", "mean", "variance", "standard deviation", "min value", "max value", "0.25 Quantile",
+                         "0.5 Quantile", "0.75 Quantile"]
+    print("Рассчёт изначальных значений набора данных:")
+    calculating_dataset_characteristics(df, table)
+    table.clear_rows()
+
+    while True:
+        try:
+            choice_normalization_type = int(input("Выберите тип нормализации (1: min-max, 2: z-ормализация): "))
+            if choice_normalization_type == 1 or choice_normalization_type == 2:
+                break
+            else:
+                print("Введите значения 1 или 2")
+        except ValueError:
+            print("шибка вы ввели неверный параметр")
+
+
+    for column in df.columns:
+        if choice_normalization_type == 1:
+            df[column] = min_max_normalize(df[column])
+        elif choice_normalization_type == 2:
+            df[column] = z_normalize(df[column])
+
+
+    print("Изменение характеристик после нормализации данных")
+    table = PrettyTable()
+    table.field_names = ["column", "mean", "variance", "standard deviation", "min value", "max value", "0.25 Quantile",
+                         "0.5 Quantile", "0.75 Quantile"]
+
+    calculating_dataset_characteristics(df, table)
+
+main()
